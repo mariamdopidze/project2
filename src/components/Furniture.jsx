89ae0e89ec2d717aa307e1d19cca13450f20e21a
mariamdopidze <mariam.dopidze@gmail.com>
2025-03-18 23:33:44 +0400

    update main page

--- src/components/Furniture.jsx
+++ src/components/Furniture.jsx
@@ -4,56 +4,61 @@ const Furniture = () => {
   return (
     <div className="max-w-[1440px] mx-auto relative mt-[83px] h-[780px]">
       
- 
-      
 
+      
+ 
       <img
         src="/images/Rectangle 36.png"
         alt=""
-        className="transform transition-transform duration-300 hover:scale-105"
+        className="top-[50px] "
       />
-      <img
-        src="/images/Rectangle 37.png"
-        alt=""
-        className="mt-4 transform transition-transform duration-300 hover:scale-105"
-      />
-
-      <img
-        src="/images/Rectangle 40.png"
-        alt=""
-        className=" absolute left-[872px] bottom-0  transform transition-transform duration-300 hover:scale-105 "
-      />
-      <img
+       <img
         src="/images/Rectangle 38.png"
         alt=""
-        className="absolute left-[95px] top-[150px] transform transition-transform duration-300 hover:scale-105"
+        className="absolute left-[95px] top-[70px] "
       />
-      <img
-        src="/images/Rectangle 41.png"
+
+<img
+        src="/images/Rectangle 40.png"
         alt=""
-        className=" absolute left-[875px] top-[150px]  transform transition-transform duration-300 hover:scale-105 "
+        className=" absolute left-[570px] bottom-[200px]   "
       />
-      <img
+
+<img
         src="/images/Rectangle 43.png"
         alt=""
-        className=" absolute right-0 top-[120px]  transform transition-transform duration-300 hover:scale-105 "
+        className=" absolute right-[270px] top-[120px] "
       />
       <img
         src="/images/Rectangle 45.png"
         alt=""
-        className=" absolute right-[116px] bottom-0  transform transition-transform duration-300 hover:scale-105 "
+        className=" absolute left-[1190px] bottom-[310px]"
       />
+     
+
+      
+      {/* 
+
+     
+     
+      <img
+        src="/images/Rectangle 41.png"
+        alt=""
+        className=" absolute left-[875px] top-[150px]   "
+      />
+     
+     
       <img
         src="/images/Rectangle 39.png"
         alt=""
-        className=" absolute left-[560px] top-[200px]  transform transition-transform duration-300 hover:scale-105 "
+        className=" absolute left-[560px] top-[200px]   "
       />
 
       <img
         src="/images/Rectangle 44.png"
         alt=""
-        className=" absolute bottom-[60px] left-[200px]  transform transition-transform duration-300 hover:scale-105 "
-      />
+        className=" absolute bottom-[60px] left-[200px]  "
+      />  */}
     </div>
   );
 };
