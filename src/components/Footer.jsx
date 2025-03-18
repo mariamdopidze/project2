be7d316142e81296e47fe5a283c1cabc5ff1d66c
mariamdopidze <mariam.dopidze@gmail.com>
2025-03-18 22:34:22 +0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,26 +4,28 @@ const Footer = () => {
   return (
     <div className="max-w-[1440px] h-[505px] w-full mx-auto bg-white">
       <footer className="bg-white dark:bg-gray-900">
-      <hr className="my-6 border-gray-200" />
-        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <hr className="my-6 border-gray-200 mt-[8px]" />
+        <div className="mx-auto w-[1240px] h-[419px] max-w-screen-xl p-4 py-6 lg:py-8 mt-[48px]">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Funiro
+                <span class="self-center text-2xl font-bold whitespace-nowrap dark:text-white ">
+                  Funiro.
                 </span>
               </a>
-              <span>400 University Dirve Suite 200 Coral </span>
-              <span>Gables</span>
-              <span>FL 33134 USA</span>
+              <div className="flex flex-col mt-[50px] text-base text-[#9F9F9F] ">
+                <span className="">400 University Dirve Suite 200 Coral </span>
+                <span>Gables</span>
+                <span>FL 33134 USA</span>
+              </div>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <h2 class="mb-6 text-base font-semibold text-[#9F9F9F] uppercase dark:text-white">
                   Links
                 </h2>
-                <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                  <li class="mb-4">
+                <ul class="text-black dark:text-gray-400 font-medium text-base space-y-8">
+                  <li>
                     <a href="https://flowbite.com/" class="hover:underline">
                       Home
                     </a>
@@ -45,12 +47,13 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
+
               <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <h2 class="mb-6 text-base font-semibold text-[#9F9F9F] uppercase dark:text-white">
                   Help
                 </h2>
-                <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                  <li class="mb-4">
+                <ul class="text-black dark:text-gray-400 font-medium text-base space-y-8">
+                  <li class="mt-[48px]">
                     <a
                       href="https://github.com/themesberg/flowbite"
                       class="hover:underline "
@@ -81,24 +84,20 @@ const Footer = () => {
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                   Newsletters
                 </h2>
-                <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                  <li class="mb-4">
-                    <a href="#" class="hover:underline">
-                      Privacy Policy
-                    </a>
+                <ul class="  space-y-6">
+                  <li class="mt-[48px] flex gap-6">
+                    <button className="border-b text-sm font-normal text-[#9F9F9F] border-black gap-4">Enter Your Email Address</button>
+                    <button className="border-b border-black">SUBSCRIBE</button>
+                    
                   </li>
-                  <li>
-                    <a href="#" class="hover:underline">
-                      Terms &amp; Conditions
-                    </a>
-                  </li>
+                 
                 </ul>
               </div>
             </div>
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <span class="text-black text-base sm:text-center dark:text-gray-400">
               © 2023{" "}
               <a href="https://flowbite.com/" class="hover:underline">
                 Funiro
